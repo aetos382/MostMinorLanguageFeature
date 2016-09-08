f067be5b359d485f403d82a824852b5116ab43e4
aetos <aetos@aerie.jp>
2016-09-08 17:04:37 +0900

    initial prototype

--- MostMinorLanguageFeature/Program.cs
+++ MostMinorLanguageFeature/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Octokit;
 
 namespace MostMinorLanguageFeature
 {
@@ -10,6 +12,67 @@ namespace MostMinorLanguageFeature
     {
         static void Main(string[] args)
         {
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("MMLF", "1.0"));
+
+            var searchRepoRequest = new SearchRepositoriesRequest
+            {
+                Language = Language.CSharp,
+                SortField = RepoSearchSort.Stars,
+                Order = SortDirection.Descending,
+                PerPage = 100
+            };
+
+            var repos = await client.Search.SearchRepo(searchRepoRequest);
+
+            foreach (var repo in repos.Items)
+            {
+                var searchCodeRequest = new SearchCodeRequestFix
+                {
+                    Language = Language.CSharp,
+                    PerPage = 100
+                };
+
+                searchCodeRequest.Repos.Add(repo.FullName);
+
+                for (int page = 0;; ++page)
+                {
+                    searchCodeRequest.Page = page;
+
+                    var codes = await client.Search.SearchCode(searchCodeRequest);
+
+                    foreach (var code in codes.Items)
+                    {
+                        var contents = await client.Repository.Content.GetAllContents(repo.Id, code.Path);
+
+                        foreach (var content in contents)
+                        {
+                            var ast = CSharpSyntaxTree.ParseText(content.Content);
+                            var root = await ast.GetRootAsync();
+
+                            var counts = root.DescendantTokens()
+                                .Where(x => x.IsKeyword())
+                                .GroupBy(x => x.Kind())
+                                .Select(x => new
+                                {
+                                    Keyword = x.Key,
+                                    Count = x.Count()
+                                })
+                                .OrderByDescending(x => x.Count)
+                                .ToArray();
+
+                            foreach (var count in counts)
+                            {
+                                Console.WriteLine($"{count.Keyword,-20}\t{count.Count,10}");
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ MostMinorLanguageFeature/SearchCodeRequestFix.cs
@@ -0,0 +1,28 @@
+﻿namespace MostMinorLanguageFeature
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Octokit;
+
+    internal class SearchCodeRequestFix : SearchCodeRequest
+    {
+        public override IReadOnlyList<string> MergedQualifiers()
+        {
+            var result = base.MergedQualifiers();
+            var parameters = new List<string>(result.Where(x => !x.StartsWith("language:")));
+
+            if (this.Language != null)
+            {
+                parameters.Add(string.Format(CultureInfo.InvariantCulture, "language:{0}", this.Language));
+            }
+
+            return new ReadOnlyCollection<string>(parameters);
+        }
+    }
+}
